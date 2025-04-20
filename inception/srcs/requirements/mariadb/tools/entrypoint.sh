85b95e5f85234db484b5260877525f623961b0b0
InGrid <indynha.w@gmail.com>
2025-04-20 19:13:32 -0300

    feat: mariadb with sql template

--- inception/srcs/requirements/mariadb/tools/entrypoint.sh
+++ inception/srcs/requirements/mariadb/tools/entrypoint.sh
@@ -3,10 +3,12 @@
 # Encerra o script se ocorrer qualquer erro (fail-fast)
 set -e
 
-# envsubst < init.sql.template > init.sql
+# Substitui as variáveis de ambiente
+envsubst '$WP_DATABASE_NAME $WP_DATABASE_USER $WP_DATABASE_PASSWORD $WP_DATABASE_ROOT_PASSWORD $HEALTH_USER $HEALTH_PASS' < /etc/init.sql.template > /etc/init.sql
+rm /etc/init.sql.template
 
 # Garante que o diretório de dados do MariaDB está inicializado
-echo "[MariaDB] Iniciando servidor em segundo plano..."
+echo "[MariaDB] Starting server in the background..."
 if [ ! -d "/var/lib/mysql/mysql" ]; then
     echo "[MariaDB] Initializing data directory..."
     mariadb-install-db --user=mysql --datadir=/var/lib/mysql
@@ -27,28 +29,29 @@ if mariadb --protocol=socket -u root -e "USE \`${WP_DATABASE_NAME}\`;" 2>/dev/nu
   echo "Database '${WP_DATABASE_NAME}' already exists. Skipping initialization."
 else
   echo "[MariaDB] Setting up database and users..."
-  mariadb --protocol=socket -u root <<EOF
-  -- Remove usuário anônimo e banco de testes
-  DROP USER IF EXISTS ''@'localhost';
-  DROP DATABASE IF EXISTS test;
+  mariadb --protocol=socket -u root < /etc/init.sql
+#   mariadb --protocol=socket -u root <<EOF
+#   -- Remove usuário anônimo e banco de testes
+#   DROP USER IF EXISTS ''@'localhost';
+#   DROP DATABASE IF EXISTS test;
 
-  -- Cria o banco de dados do WordPress (se não existir)
-  CREATE DATABASE IF NOT EXISTS \`${WP_DATABASE_NAME}\`;
+#   -- Cria o banco de dados do WordPress (se não existir)
+#   CREATE DATABASE IF NOT EXISTS \`${WP_DATABASE_NAME}\`;
 
-  -- Cria o usuário de aplicação e concede permissões
-  CREATE USER IF NOT EXISTS '${WP_DATABASE_USER}'@'%' IDENTIFIED BY '${WP_DATABASE_PASSWORD}';
-  GRANT ALL PRIVILEGES ON \`${WP_DATABASE_NAME}\`.* TO '${WP_DATABASE_USER}'@'%';
+#   -- Cria o usuário de aplicação e concede permissões
+#   CREATE USER IF NOT EXISTS '${WP_DATABASE_USER}'@'%' IDENTIFIED BY '${WP_DATABASE_PASSWORD}';
+#   GRANT ALL PRIVILEGES ON \`${WP_DATABASE_NAME}\`.* TO '${WP_DATABASE_USER}'@'%';
 
-  -- Configura senha do usuário root
-  ALTER USER 'root'@'localhost' IDENTIFIED BY '${WP_DATABASE_ROOT_PASSWORD}';
+#   -- Configura senha do usuário root
+#   ALTER USER 'root'@'localhost' IDENTIFIED BY '${WP_DATABASE_ROOT_PASSWORD}';
 
-  -- Usuário para healthcheck
-  CREATE USER IF NOT EXISTS '${HEALTH_USER}'@'localhost' IDENTIFIED BY '${HEALTH_PASS}';
-  GRANT USAGE ON *.* TO '${HEALTH_USER}'@'localhost';
+#   -- Usuário para healthcheck
+#   CREATE USER IF NOT EXISTS '${HEALTH_USER}'@'localhost' IDENTIFIED BY '${HEALTH_PASS}';
+#   GRANT USAGE ON *.* TO '${HEALTH_USER}'@'localhost';
 
-  -- Aplica todas as mudanças
-  FLUSH PRIVILEGES;
-EOF
+#   -- Aplica todas as mudanças
+#   FLUSH PRIVILEGES;
+# EOF
 fi
 
 # Finaliza o servidor temporário
