90adca1624903316c8994f9a4953055af850029b
InGrid <indynha.w@gmail.com>
2025-04-21 10:37:28 -0300

    feat: rename template

--- inception/srcs/requirements/nginx/tools/entrypoint.sh
+++ inception/srcs/requirements/nginx/tools/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-envsubst '$DOMAIN_NAME' < /etc/nginx/nginx-template.conf > /etc/nginx/nginx.conf
+envsubst '$DOMAIN_NAME' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 # Inicia o Nginx em primeiro plano (não como daemon), para que o Docker possa manter o container ativo
 exec nginx -g 'daemon off;'  # 'daemon off' é necessário no Docker para garantir que o Nginx rode em primeiro plano
