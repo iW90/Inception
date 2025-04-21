44448c7e2c7e8b97c603059d5ad5501da9cd3d56
InGrid <indynha.w@gmail.com>
2025-04-21 10:18:12 -0300

    style: organize comments

--- inception/srcs/requirements/nginx/tools/entrypoint.sh
+++ inception/srcs/requirements/nginx/tools/entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-# Substitui as variáveis de ambiente dentro do arquivo de template para criar o arquivo de configuração do Nginx
 envsubst '$DOMAIN_NAME' < /etc/nginx/nginx-template.conf > /etc/nginx/nginx.conf
-# O comando acima vai ler o template de configuração nginx-template.conf,
-# substituir a variável $DOMAIN_NAME pelo seu valor atual e salvar o resultado em nginx.conf.
 
 # Inicia o Nginx em primeiro plano (não como daemon), para que o Docker possa manter o container ativo
 exec nginx -g 'daemon off;'  # 'daemon off' é necessário no Docker para garantir que o Nginx rode em primeiro plano
